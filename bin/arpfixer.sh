4972f3e1d0f92c231c8a79d33de9ac10fef14130
inspired <mikael@bjerkeland.com>
2016-11-15 16:20:33 +0100

    Update arpfixer.sh

--- bin/arpfixer.sh
+++ bin/arpfixer.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 tempfile=`mktemp`
 tempfile2=`mktemp`
-tempfile3=`mktemp`
+destination_file=/home/s_arpdump/FW_NAME_arp.csv
+excluded_macs=/home/s_arpdump/FW_NAME_localadd
 
-
-echo "Temp files $tempfile $tempfile2"
-cat /home/s_arpdump/FW_NAME_arp.txt | sed s/" "/,/g | sed  $'s/\t//'  > $tempfile
-tail -n +2 $tempfile | sed '1s/^/src_interface,src_ip,src_mac,age\n/' >/home/s_arpdump/FW_NAME_arp.csv
+cat /home/s_arpdump/byfw01.gassco.no_arp.txt | sed s/" "/,/g | sed  $'s/\t//'  > $tempfile
+tail -n +2 $tempfile | sed '1s/^/src_interface,src_ip,src_mac,age\n/' >$tempfile2
+if [ -f $excluded_macs ];
+then
+        # Remove local addresses
+        grep -vwF -f $excluded_macs $tempfile2 > $destination_file
+else
+        # Just copy to destination
+        cp $tempfile2 $destination_file
+fi
 
 rm $tempfile
 rm $tempfile2
-rm $tempfile3
